4b4d938f0b80f486b8ba4ae7fe26057e1a971fb2
Brice <bricemfrisco@gmail.com>
2022-01-01 18:06:34 -0600

    Add sidebar navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import {Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar} from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import StorageIcon from '@mui/icons-material/Storage';
+import NetworkCheckIcon from '@mui/icons-material/NetworkCheck';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import { makeStyles } from '@mui/styles';
+import {Link, useLocation} from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+    drawer: { // Side navigation bar
+      width: theme.navbar.width,
+      flexShrink: 0,
+      '& .MuiDrawer-paper': {
+          width: theme.navbar.width,
+          boxSizing: 'border-box',
+          backgroundColor: theme.palette.background.paper
+      }
+    },
+    list: { // Side navigation items
+        '& .MuiTypography-root': { // Side navigation typography
+          fontWeight: 'bold',
+          color: theme.palette.text.secondary,
+          fontSize: '0.85rem',
+        },
+        '& .MuiSvgIcon-root': { // Side navigation icons
+            color: theme.palette.text.secondary
+        },
+        '& .MuiButtonBase-root:hover': { // Side navigation button on hover
+            backgroundColor: 'inherit',
+            cursor: 'pointer',
+
+            '& .MuiTypography-root': { // Side navigation typography on hover
+                color: theme.palette.text.primary,
+                transition: theme.transitions.create(['color'], {
+                    duration: 500
+                }),
+            },
+
+            '& .MuiSvgIcon-root': { // Side navigation icons on hover
+                color: theme.palette.text.primary,
+                transition: theme.transitions.create(['color'], {
+                    duration: 500
+                }),
+            }
+        },
+        '& .MuiButtonBase-root.Mui-selected': { // Selected side navigation
+            '& .MuiTypography-root': {
+                color: theme.palette.text.primary,
+            },
+            '& .MuiSvgIcon-root': {
+                color: theme.palette.text.primary,
+            }
+        },
+        '& .MuiButtonBase-root.Mui-selected:hover': { // Selected side navigation on hover
+            backgroundColor: 'rgba(144, 202, 249, 0.16)'
+        },
+    },
+    link: { // React router links
+        textDecoration: 'none'
+    }
+}))
+
+const Navigation = () => {
+    const classes = useStyles();
+    const location = useLocation();
+
+    return (
+        <Drawer className={classes.drawer} variant='permanent' anchor='left'>
+            <Toolbar>MineCount</Toolbar>
+            <Divider />
+            <List className={classes.list}>
+                <Link to='/' className={classes.link}>
+                    <ListItem button disableRipple selected={location.pathname === '/'} key='Home'>
+                        <ListItemIcon>
+                            <HomeIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='Home' />
+                    </ListItem>
+                </Link>
+
+                <Link to='/servers' className={classes.link}>
+                    <ListItem button disableRipple selected={location.pathname === '/servers'} key='Servers'>
+                        <ListItemIcon>
+                            <StorageIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='Servers'/>
+                    </ListItem>
+                </Link>
+
+                <Link to='/ping' className={classes.link}>
+                    <ListItem button disableRipple selected={location.pathname === '/ping'} key='Ping'>
+                        <ListItemIcon>
+                            <NetworkCheckIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='Ping' />
+                    </ListItem>
+                </Link>
+
+                <Link to='/admin' className={classes.link}>
+                    <ListItem button disableRipple selected={location.pathname === '/admin'} key='Admin'>
+                        <ListItemIcon>
+                            <AdminPanelSettingsIcon />
+                        </ListItemIcon>
+                        <ListItemText primary='Admin' />
+                    </ListItem>
+                </Link>
+            </List>
+        </Drawer>
+    )
+}
+
+export default Navigation;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {CssBaseline} from "@mui/material";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Navigation from "./components/Navigation";
 
-import './index.css';
+const theme = createTheme({
+    typography: {
+        fontFamily: 'Roboto'
+    },
+    palette: {
+        mode: 'dark',
+        background: {
+            default: '#181616'
+        },
+    },
+    navbar: {
+        width: '240px'
+    }
+})
 
 ReactDOM.render(
     <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<App />} />
-            <Route path='/admin' element={<div>admin</div>} />
-            <Route path='/add-server' element={<div>add-server</div>} />
-            <Route path='/ping' element={<div>ping</div>} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <Navigation />
+            <Routes>
+                <Route path='/' element={<App/>}/>
+                <Route path='/servers' element={<div>add-server</div>}/>
+                <Route path='/ping' element={<div>ping</div>}/>
+                <Route path='/add-server' element={<div>add-server</div>}/>
+                <Route path='/admin' element={<div>admin</div>}/>
+            </Routes>
+        </ThemeProvider>
     </BrowserRouter>,
-  document.getElementById('root')
+    document.getElementById('root')
 );
